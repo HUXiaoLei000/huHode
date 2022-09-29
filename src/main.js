f1cff213e9d9897ecee59aa21f3cfe44db57f64c
hu <1182830389@qq.com>
2022-09-29 09:54:08 +0800

    style(custom): 配置跨域，基础配置，下载依赖

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 配置axios
+import axios from "axios"
+// 挂载原型
+Vue.prototype.axios = axios
+// 配置ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,38 @@
-const { defineConfig } = require('@vue/cli-service')
+const {
+  defineConfig
+} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  // 处理白屏
+  publicPath: "./",
+  // 端口号更改
+  host: 8889,
+  // 主机名
+  host: "localhost",
+  // 启动自动打开浏览器
+  open: true,
+  // 本地跨域处理
+  proxy: {
+    // 代理名称
+    [process.env.VUE_APP_BASE_API]: {
+      // 代理地址
+      target: process.env.VUE_APP_SERVICE_URL,
+      // 开启跨域
+      changOrigin: true,
+      // 重写路径
+      pathRewrite: {
+        ["^" + process.env.VUE_APP_BASE_API]: ""
+      }
+    }
+  },
+
+  // 关闭格式检查
+  lintOnSave: false,
+  // 打包不会生成  .map  文件
+  productionSourceMap: false
+
+
+
+
 })
